326eb48eea43dc210a8768965913fe24a68fe8cf
kobydj <kobyjensen@gmail.com>
2024-09-26 15:48:11 -0600

    fix lint again

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -2,16 +2,14 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-const badUser = { name: null, email: 'reg@test.com', password: 'a' };
 
-let testUserAuthToken;
+//let testUserAuthToken;
 
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    const registerRes = await request(app).post('/api/auth').send(testUser);
-    testUserAuthToken = registerRes.body.token;
-    const loginRes = await request(app).put('/api/auth').send(testUser);
-    testUserAuthToken = loginRes.body.token;
+    await request(app).post('/api/auth').send(testUser);
+    //testUserAuthToken = registerRes.body.token;
+    await request(app).put('/api/auth').send(testUser);
 
   });
   test('get franchise', async () => {
